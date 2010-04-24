bf9e6ba9452ebd2099fd96cea53af28da157f4d4
Rıdvan Örsvuran <flasherdn@gmail.com>
2010-04-24 18:57:40 +0300

    added setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,28 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import glob
+
+from setuptools import setup
+i = [x for x in range(10)]
+def get_icons():
+    icons = []
+    for size in glob.glob('icons/hicolor/*'):
+        for folder in glob.glob(size+'/*'):
+            icons.append(('share/'+folder, glob.glob(folder+'/*.png')))
+    return icons
+
+data_files = [
+    ("share/applications", ["data/service-manager-gtk.desktop"])
+]
+data_files.extend(get_icons())
+
+setup(name="pardus-service-manager-gtk",
+      version="0.1",
+      package_dir={"":"src"},
+      packages = ["service_manager_gtk", "asma.addons"],
+      scripts = ["src/service-manager-gtk.py"],
+      description= "Pardus Service Manager's gtk port",
+      author="Rıdvan Örsvuran",
+      author_email="flasherdn@gmail.com",
+      data_files=data_files
+)
